c9f996c5b51028814968ee24e10c5bbc935b3241
Scott Dee <sdeeut69@gmail.com>
2019-01-23 14:57:38 -0700

    Add files via upload

---
+++ Crack the code/CTCJS.js
@@ -0,0 +1,4 @@
+setTimeout(wakeUpUser, 5000);
+	function wakeUpUser() {
+	alert('are you going to stare at this boring page forever?');
+	}
---
+++ thingamajig/thingamajig.js
@@ -0,0 +1,36 @@
+//THE THING-A-MA-JIG!
+// function clunk(times) {
+//     var num = times;
+//     while (num > 0) {
+//       display("clunk");
+//       num = num -1;
+//     }
+//   }
+//    function thingamajig(size) {
+//      var facky = 1;
+//      clunkCounter = 0;
+//      if (size == 0){
+//        dislplay("clank");
+//      }else if (size == 1) {
+//        display("thunk");
+//      }else {
+//        while (size > 1) {
+//          facky = facky * size;
+//          size = size -1;
+//        }
+//        clunk(facky);
+//      }
+//      }
+  
+//      function diplay(output) {
+//        console.log(output);
+//        clunkCounter = clunkCounter + 1;
+//      }
+//      var clunkCounter = 0;
+//      thingamajig(5);
+//      console.log(clunkCounter);
+
+var scores = [60,50,60,58,54,54,58,50,52,54];
+
+var solution2 = scores[2];
+alert("Solution 2 produced " + solution2 + " bubbles.");
